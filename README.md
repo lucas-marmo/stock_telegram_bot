50126d7b5a9a216bf54c173c2f2d8c5d04fe302c
Lucas Marmo <lmarmo@frba.utn.edu.ar>
2024-01-17 21:13:25 -0300

    fixed bugs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# stock_telegram_bot
+# Stock Telegram Bot
+This project aims to use a Telegram Bot to send stock information by using a Flask App y Python. At the same time it sends information by Telegram, it also shows a user interface where you can select which company's stock price you want to know.
+
+### How to run
+Add on the root a `config.py` file with the following keys:
+
+TOKEN = `'string'`
+
+CHAT_ID = `integer`
+
+To know your bot's token, ask it to @BotFather
+To know your chat's id, ask it to @useridinfobot by sending the command `/start`
--- flask_app.py
+++ flask_app.py
@@ -1,24 +1,28 @@
+import datetime
 from flask import Flask, render_template, request, url_for
 from telegram import Update, ForceReply, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
 import yfinance as yf
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py', silent=True)
 
 @app.route('/')
 async def hello_world():
-    bot = Bot(token='COMPLETAR')
-    chat = #COMPLETAR
+    bot = Bot(token=app.config['TOKEN'])
+    chat = app.config['CHAT_ID']
     #hola = await bot.sendMessage(chat_id=chat, text='Hola desde Flask!')
     return render_template("index.html")
 
 @app.route('/precio/<accion>')
 async def precio(accion):
-    bot = Bot(token='COMPLETAR')
-    chat = #COMPLETAR
-    data = yf.download(accion, start="2023-07-31", end="2023-08-01")
-    valor = str(data['Close'].iloc[-1])
-    mensaje = f"El valor actual de {accion} es: {valor}"
+    bot = Bot(token=app.config['TOKEN'])
+    chat = app.config['CHAT_ID']
+    fecha_inicio = (datetime.datetime.today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+    fecha_fin = datetime.datetime.today().strftime('%Y-%m-%d')
+    data = yf.download(accion, start=fecha_inicio, end=fecha_fin)
+    valor = str(round(data['Close'].iloc[-1], 2))
+    mensaje = f"El valor actual de {accion} es u$s{valor}"
     hola = await bot.sendMessage(chat_id=chat, text=mensaje)
     return render_template("index.html")
 
